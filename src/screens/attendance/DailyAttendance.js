10da7876f5ac96363f5e8210c46e79c0fda3d87a
Aryan <ahujaaryan2511@gmail.com>
2023-06-27 16:51:32 +0530

    Creating seperate styles files

--- src/screens/attendance/DailyAttendance.js
+++ src/screens/attendance/DailyAttendance.js
@@ -10,6 +10,7 @@ import {
 import { useAppSelector } from '../../../store/hook';
 import firestore from '@react-native-firebase/firestore';
 import DropDownPicker from 'react-native-dropdown-picker';
+import styles from './DailyAttendance.styles';
 
 const DailyAttendance = ({ navigation }) => {
     const user = useAppSelector(state => state.profile.data);
@@ -41,8 +42,6 @@ const DailyAttendance = ({ navigation }) => {
         setCurrentIndex(0);
         fetchSubjects();
         getStudents();
-        // console.log(subject)
-
     }, [])
 
     const getStudents = async () => {
@@ -127,82 +126,5 @@ const DailyAttendance = ({ navigation }) => {
     )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        padding: 16,
-        backgroundColor: '#912929'
-    },
-    title: {
-        fontSize: 35,
-        fontWeight: 'bold',
-        marginBottom: 15,
-        color: '#fbe98d'
-    },
-    label: {
-        fontSize: 16,
-        fontWeight: 'bold',
-        marginRight: 8,
-        marginTop: 10,
-        color: 'white'
-    },
-    card: {
-        width: 300,
-        height: 300,
-        marginTop: 100,
-        alignItems: 'center',
-        padding: 20
-    },
-    heading: {
-        fontSize: 30,
-        fontWeight: 'bold',
-        marginBottom: 8,
-        color: 'white'
-    },
-    heading1: {
-        fontSize: 35,
-        fontWeight: 'bold',
-        marginBottom: 8,
-        color: '#fdf1b7'
-    },
-    buttonP: {
-        backgroundColor: 'green',
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        borderRadius: 5,
-        marginBottom: 10,
-        marginHorizontal: 10
-    },
-    buttonA: {
-        backgroundColor: 'red',
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        borderRadius: 5,
-        marginBottom: 10,
-        marginHorizontal: 10
-    },
-    buttonG: {
-        backgroundColor: 'blue',
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        borderRadius: 5,
-        marginBottom: 10,
-        marginHorizontal: 10
-    },
-    buttonText: {
-        color: 'white',
-        fontSize: 16,
-        fontWeight: 'bold',
-    },
-    picker: {
-        height: 40,
-        backgroundColor: 'white',
-        borderColor: '#ccc',
-        borderWidth: 1,
-        borderRadius: 4,
-        marginBottom: 20,
-    },
-});
 
 export default DailyAttendance;
