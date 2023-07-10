13b8f3c3fd203a060e2f106ebc532def145efc00
krupaw <co2021.krupa.waghela@ves.ac.in>
2023-07-10 19:49:26 +0530

    made changes in functionality of enquiry management

--- src/screens/enquirymanagement/Enquirymanagement.js
+++ src/screens/enquirymanagement/Enquirymanagement.js
@@ -33,12 +33,10 @@ export default Enquiry;
 
 
 
-export const Query = () => {
+/*export const Query = () => {
   const [type, setType] = useState('');
   const [description, setDescription] = useState('');
   const [showQueryType, setShowQueryType] = useState(false);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const queryTypes = ['Fee related Query', 'Admission related Query', 'Syllabus Related Query', 'Another Query Type'];
   const user = useAppSelector(state => state.profile.data);
 
@@ -52,16 +50,14 @@ export const Query = () => {
   };
 
   const submitData = async () => {
-    if (!type || !description || !email || !password) {
-      Alert.alert("error", "Please enter email and password, write your Query");
+    if (!type || !description) {
+      Alert.alert("error", "write your Query");
     } else {
         await firestore().collection('Query').add({
           Email: user.email,
           Type: type,
           Description: description,
         });
-        setEmail('');
-        setPassword('');
         setType('');
         setDescription('');
         Alert.alert('Query submitted');
@@ -115,33 +111,127 @@ export const Query = () => {
     </KeyboardAvoidingView>
 
   );
+};*/
+
+export const Query = () => {
+  const [type, setType] = useState('');
+  const [description, setDescription] = useState('');
+  const [showQueryType, setShowQueryType] = useState(false);
+  const [selectedQueryType, setSelectedQueryType] = useState('');
+
+  const queryTypes = ['Fee related Query', 'Admission related Query', 'Syllabus Related Query', 'Another Query Type'];
+  const user = useAppSelector(state => state.profile.data);
+
+  const toggleQueryType = () => {
+    setShowQueryType(!showQueryType);
+  };
+
+  const handleQueryTypeSelection = (selectedType) => {
+    setType(selectedType);
+    setSelectedQueryType(selectedType);
+    setShowQueryType(false);
+  };
+
+  const submitData = async () => {
+    if (!type || !description) {
+      Alert.alert("Error", "Please provide both the query type and description.");
+    } else {
+      let queryTypeToSend = type;
+      if (type === "Another Query Type") {
+        if (!selectedQueryType) {
+          Alert.alert("Error", "Please write your query type.");
+          return;
+        }
+        queryTypeToSend = selectedQueryType;
+      }
+      
+      await firestore().collection('Query').add({
+        Email: user.email,
+        Type: queryTypeToSend,
+        Description: description,
+      });
+      setType('');
+      setDescription('');
+      setSelectedQueryType('');
+      Alert.alert('Query submitted');
+    }
+  };
+
+  return (
+    <KeyboardAvoidingView style={styles.container}>
+      <View>
+        <View style={styles.sectionContainer}>
+          <View style={styles.sectionHeader}>
+            <TouchableOpacity onPress={toggleQueryType}>
+              <Text style={styles.heading}>
+                Query Type
+                {showQueryType ? <Text style={styles.arrowIcon}>▲</Text> : <Text style={styles.arrowIcon}>▼</Text>}
+              </Text>
+            </TouchableOpacity>
+          </View>
+          {showQueryType && (
+            <View style={styles.queryTypeContainer}>
+              {queryTypes.map((queryType) => (
+                <TouchableOpacity
+                  key={queryType}
+                  onPress={() => handleQueryTypeSelection(queryType)}
+                >
+                  <Text style={[styles.queryType, selectedQueryType === queryType ? styles.selectedQueryType : null]}>{queryType}</Text>
+                </TouchableOpacity>
+              ))}
+            </View>
+          )}
+        </View>
+        {selectedQueryType === 'Another Query Type' && (
+          <TextInput
+            name="CustomQueryType"
+            style={styles.QueryTypeinput}
+            placeholder="Write your query type"
+            value={type}
+            onChangeText={value => setType(value)}
+            placeholderTextColor="black"
+          />
+        )}
+        <TextInput
+          name="Description"
+          style={styles.input}
+          placeholder="Description"
+          value={description}
+          onChangeText={value => setDescription(value)}
+          placeholderTextColor="black"
+          multiline
+          numberOfLines={6}
+        />
+
+        <TouchableOpacity
+          onPress={submitData}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Submit</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
 };
+
+
 
 export const Feedback = () => {
   const [Description, setFeedbackDescription] = useState("");
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
 
   const user = useAppSelector(state => state.profile.data);
 
   const submitData = async () => {
-    if (!Description || !email || !password) {
-      Alert.alert("error", "Please enter email and password, write your feedback");
+    if (!Description) {
+      Alert.alert("error", " write your feedback");
     } else {
-      if (user.email === '' && user.password=== '') {
       const feedbackData = {
         Email: user.email,
         Description: Description
       };
       await firestore().collection("Feedback").add(feedbackData);
-      setEmail('');
-      setPassword('');
       setFeedbackDescription("");
       Alert.alert("Feedback submitted");
-    }  
-    else {
-      Alert.alert("Error", "User not Found")
-  }
 }
   }
   const handleFeedbackChange = (value) => {
